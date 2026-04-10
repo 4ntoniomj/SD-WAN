ae2b4b2d3931a23f5d10f076b69c892cc80e0afe
Antonio Madrid <mj.antonio.madrid@gmail.com>
2026-04-10 23:34:35 +0200

    Second wireguard

--- gekey.sh
+++ gekey.sh
@@ -4,3 +4,7 @@ names=("hq" "sede1" "sede2" "sede3" "sede4")
 for i in ${names[@]}; do
 	wg genkey | tee "${i}".key | wg pubkey > "${i}.pub"
 done
+
+for i in ${names[@]}; do
+	wg genkey | tee "${i}".2.key | wg pubkey > "${i}.2.pub"
+done
